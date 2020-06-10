1fbc9189d5ef84d7790c8ecb6e4c3f9b27ef7dbd
haleyysanchez <haleymsanchez910@gmail.com>
2020-06-10 10:32:40 -0400

    Initial Commit

---
+++ BLM_App_tabs/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BLM_App_tabs
+//
+//  Created by Haley Sanchez on 6/10/20.
+//  Copyright © 2020 Haley Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
